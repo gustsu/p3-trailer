f745e995050f96c9039ad78c4b5848727f39ef22
gustsu <justintew@eagles.ewu.edu>
2015-05-20 08:35:28 -0700

    added video player plug-in

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 // My Scripts
 
 //document.write("Hello World!");
+
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+});
+
+
+
+
+
+
+
