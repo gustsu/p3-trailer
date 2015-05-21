6554ea1f809905d670325b5b2c35bd130515ff18
gustsu <justintew@eagles.ewu.edu>
2015-05-20 22:29:27 -0700

    added nav

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,14 @@ $('audio,video').mediaelementplayer({
 	success: function(player, node) {
 	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
 	},
+    features:['playpause','progress','fullscreen'],
 	startLanguage: 'en',
 	translationSelector: true
 });
+
+
+$("#mainlogo").hide();
+$("#mainlogo").fadeIn(4000);
 
 
 
